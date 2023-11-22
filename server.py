ac2768ae525ff08d2066b028977bde4ef4bf5107
Mohammed M M Alshamrani <81635744+MohammedMM2020@users.noreply.github.com>
2023-11-23 00:31:58 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,11 @@
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 from EmotionDetection.emotion_detection import emotion_predictor
-
 app = Flask("Emotion Detection")
-
 def run_emotion_detection():
-    """
-    Main function to run the Emotion Detection application.
-    """
     app.run(host="0.0.0.0", port=5000)
-
 @app.route("/emotionDetector")
 def sent_detector():
-    """
-    Analyze the user-provided text for emotions and return the result.
-    """
     text_to_detect = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_detect)
     formated_response = emotion_predictor(response)
@@ -26,13 +17,8 @@ def sent_detector():
         f"'joy': {formated_response['joy']} and 'sadness': {formated_response['sadness']}. "
         f"The dominant emotion is {formated_response['dominant_emotion']}."
     )
-
 @app.route("/")
 def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
-    '''
     return render_template('index.html')
-
 if __name__ == "__main__":
     run_emotion_detection()
